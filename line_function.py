85e686c0d0dddfb9a50814a3e66cbb9a85618fe2
ianshimabukuro <shimakun000@gmail.com>
2024-05-07 00:28:13 +0900

    MAde it so that the function is consistent through iterations and the boundary conditions always apply before the next line

--- line_function.py
+++ line_function.py
@@ -2,25 +2,30 @@ import numpy as np
 
 
 def line_to_line_function(x):
-    size = x.size
+    print("Input size:",x.size)
     y = np.array([0,0])
     iteration = np.array([])
     j=np.array([])
 
-    for i in range(0,size-1):
-        if i!=0 or i!=size-1:
-            j[i] = x[i-1]+x[i]+5*x[i+1]
+    for i in range(1,x.size-1):
+        print(i)
+        j = np.append(j,x[i-1]+x[i]+5*x[i+1])
 
-    for a in range(0,j.size-1):
+    print("Middle size:",j.size)
+    for a in range(0,j.size):
         if j[a]%2 == 0:
-            iteration[a] = 0
+            iteration = np.append(iteration,0)
         else:
-            iteration[a] = 1
-    np.append(y,j)
-    return next
+            iteration = np.append(iteration,1)
+    print("Middle after bin:",iteration.size)
+
+    y = np.insert(y,1,iteration)
+
+    print("output size:",y.size)
+    return y
 
 
-initial = np.array([0,0,0,0,0,1,0,1,1,1,0,0,0])
+initial = np.array([0,0,0,0,0,1,0,1,1,1,0,0,0,1,1,0,0,1])
 next = line_to_line_function(initial)
 print(next)
 initial = next
